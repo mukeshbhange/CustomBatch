8c789e05accd7ca10875a6ec34aa10ff11c43317
shivjalkote <jalkoteshiv@gmail.com>
2021-12-20 14:18:51 +0530

    LMS Onboarding Mysql

---
+++ lms_candidate_onboadring/src/test/java/com/bl/onboarding/LmsCandidate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsCandidate1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
